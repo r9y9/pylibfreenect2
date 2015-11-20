4165388e17d9d16b6852cbe9199da27e36d03460
Ryuichi YAMAMOTO <yamamotoryuichi@team-lab.com>
2015-11-20 17:33:53 +0900

    Registrationも行うようにexampleを修正

--- examples/multiframe_listener.py
+++ examples/multiframe_listener.py
@@ -3,12 +3,14 @@
 import numpy as np
 import cv2
 from pylibfreenect2 import pyFreenect2, pyFrameMap, pySyncMultiFrameListener
-from pylibfreenect2 import FrameType
+from pylibfreenect2 import FrameType, pyRegistration, pyFrame
+
 
 fn = pyFreenect2()
 device = fn.openDefaultDevice()
 
-listener = pySyncMultiFrameListener(FrameType.Color | FrameType.Ir | FrameType.Depth)
+listener = pySyncMultiFrameListener(
+    FrameType.Color | FrameType.Ir | FrameType.Depth)
 
 # Register listeners
 device.setColorFrameListener(listener)
@@ -16,17 +18,27 @@ device.setIrAndDepthFrameListener(listener)
 
 device.start()
 
+# NOTE: must be called after device.start()
+registration = pyRegistration(device.getIrCameraParams(),
+                              device.getColorCameraParams())
+
+undistorted = pyFrame(512, 424, 4)
+registered = pyFrame(512, 424, 4)
+
 while True:
     frames = pyFrameMap()
     listener.waitForNewFrame(frames)
 
-    color = frames[FrameType.Color].udata()
-    ir = frames[FrameType.Ir].data()
-    depth = frames[FrameType.Depth].data()
+    color = frames["color"]
+    ir = frames["ir"]
+    depth = frames["depth"]
 
-    cv2.imshow("ir", ir / 65535.)
-    cv2.imshow("depth", depth / 4500.)
-    cv2.imshow("color", color)
+    registration.apply(color, depth, undistorted, registered)
+
+    cv2.imshow("ir", ir.data() / 65535.)
+    cv2.imshow("depth", depth.data() / 4500.)
+    cv2.imshow("color", color.udata())
+    cv2.imshow("registered", registered.udata())
 
     listener.release(frames)
 
